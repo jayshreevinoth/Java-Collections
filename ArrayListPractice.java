96ab6d64c29fa9d94769225bf9aecdbfd02a0246
jayshreevinoth <45241188+jayshreevinoth@users.noreply.github.com>
2025-01-20 14:04:57 +0000

    Add files via upload

---
+++ ArrayListPractice.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class ArrayListPractice {
+    public static void main(String[] args) {
+        List<String> str = new ArrayList<String>();
+        str.add("A");
+        str.add("B");
+        str.add("C");
+        str.add("Z");
+        str.add("A");
+        str.add("P");
+        str.add("P");
+
+        // Sorting the array
+        Collections.sort(str);
+
+        /*
+         * Remove dupicates using iterator by creating another list by storing first
+         * appearance of each element
+         */
+        ArrayList<String> temp = new ArrayList<String>();
+        for (int i = 0; i < str.size(); i++) {
+            if (!temp.contains(str.get(i))) {
+                temp.add(str.get(i));
+            }
+        }
+
+        /* Remove duplicates using LinkedHashSet */
+        LinkedHashSet<String> hashtemp = new LinkedHashSet<String>();
+        hashtemp.addAll(str);
+
+        /* Removes duplicates using streams distinct */
+        List<String> tempStream = str.stream().distinct().collect(Collectors.toList());
+    }
+}
